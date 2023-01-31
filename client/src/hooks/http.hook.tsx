c6ab17147e94e0423dd49d6949c68fe395614966
egor594bed <egor594work@gmail.com>
2023-01-31 15:36:51 +0300

    fix

--- client/src/hooks/http.hook.tsx
+++ client/src/hooks/http.hook.tsx
@@ -2,7 +2,6 @@ import { useState, useCallback } from "react"
 
 
 export const useHttp = () => {
-    const backendUrl = 'hobby-hall-backend.onrender.com'
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const request = useCallback(async (url: string, method = 'GET', body: any = null, headers: any = {}) => {
@@ -12,7 +11,7 @@ export const useHttp = () => {
                 body = JSON.stringify(body)
                 headers['Content-Type'] = 'application/json'
             }
-            const response = await fetch(backendUrl + url, {method, body, headers})
+            const response = await fetch(url, {method, body, headers})
             const data = await response.json()
 
             if (!response.ok) {
